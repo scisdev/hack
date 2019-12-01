86ce3d31661412e02532886f7b908fc32b4a9199
scisdev <KLiF249@gmail.com>
2019-12-01 11:16:45 +0500

    front-fix

--- front/pages/index.js
+++ front/pages/index.js
@@ -3,15 +3,16 @@ import LayoutHead from "../components/LayoutHead";
 import fetch from 'isomorphic-unfetch';
 import GoodCard from "../components/GoodCard";
 
+
 const Home = (props) => (
   <div>
     <LayoutHead />
     <div className="hero">
 
-        <div>
-        {props.goods.data.map(
-            (good, index) => <GoodCard key={index} {...good} />
-        )}
+        <div id="grid">
+          {props.goods.data.map(
+              (good, index) => <GoodCard key={index} {...good} />
+          )}
         </div>
     </div>
 
@@ -21,12 +22,18 @@ const Home = (props) => (
         width: 100%;
         color: #333;
       }
+      #grid {
+        display: grid;
+        grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+        grid-gap: 2vw;
+      }
     `}</style>
   </div>
 );
 
 Home.getInitialProps = async function() {
-    const res = await fetch('http://chelhack.deletestaging.com/goods');
+    //const res = await fetch('http://chelhack.deletestaging.com/goods');
+    const res = await fetch('http://localhost:8080/api');
     const data = await res.json();
 
     // console.log(`get Data `, data);
