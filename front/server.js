957e6dd0886c9e19a026feb0fc7e3c1045dc92cd
RuGrof <RuGrof@gmail.com>
2019-12-01 12:06:17 +0500

    Added pm2 cluster and fastify

---
+++ front/server.js
@@ -0,0 +1,42 @@
+const fastify = require('fastify')({logger: {level: 'error'}});
+const Next = require('next');
+
+const port = parseInt(process.env.PORT, 10) || 3000;
+const dev = process.env.NODE_ENV !== 'production';
+
+fastify.register((fastify, opts, next) => {
+    const app = Next({dev});
+    const handleRequest = app.getRequestHandler();
+
+    app
+        .prepare()
+        .then(() => {
+            if (dev) {
+                fastify.get('/_next/*', (req, reply) => {
+                    return handleRequest(req.req, reply.res).then(() => {
+                        reply.sent = true
+                    })
+                })
+            }
+
+            fastify.all('*', (req, reply) => {
+                return handleRequest(req.req, reply.res).then(() => {
+                    reply.sent = true
+                })
+            });
+
+            fastify.setNotFoundHandler((request, reply) => {
+                return app.render404(request.req, reply.res).then(() => {
+                    reply.sent = true
+                })
+            });
+
+            next()
+        })
+        .catch(err => next(err))
+})
+
+fastify.listen(port, err => {
+    if (err) throw err
+    console.log(`> Ready on http://localhost:${port}`)
+})
